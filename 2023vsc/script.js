321066f0e818ae31e1a64045442a077236d07c33
OdinLaVoie <Lexx_2_00@mail.ru>
2023-10-03 22:10:29 +0600

    mymymymy

--- 2023vsc/script.js
+++ 2023vsc/script.js
@@ -23,7 +23,7 @@ const smallestDivisor = (num) => {
 
 
 
-/////////     РАЗЛОЖЕНИЕ НА ПРОСТЫЕ МНОЖИТЕЛИ      /////////
+/////////    РАЗЛОЖЕНИЕ НА ПРОСТЫЕ МНОЖИТЕЛИ      /////////
 /**
  * Returns an array containing the prime factors of the given number.
  *
